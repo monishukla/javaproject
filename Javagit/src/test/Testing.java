cd311ea33ca7267a74122b977bea685d82793c3b
monishukla <manisha10850@gmail.com>
2020-11-01 19:55:13 +0530

    Testing class

---
+++ Javagit/src/test/Testing.java
@@ -0,0 +1,20 @@
+package test;
+
+public class Testing {
+	public static void main(String[] args) {
+		int t = 3;
+		printFun(t);
+	}
+	
+	public static void printFun(int a) {
+		if(a<1) {
+			return;
+		}else {
+			System.out.println(a);
+			printFun(a-1);
+			System.out.println(a);
+			return;
+		}
+	}
+
+}
