31c62f2305a4b98d373dbff9542de35c81bf5ed1
Manisha <manisha10850@gmail.com git config --global useer.email manisha10850@gmail.comgit config --global user.email manisha10850@gmail.com>
2021-05-22 18:10:03 +0530

    commit

---
+++ MyProject/src/factorial/Example.java
@@ -0,0 +1,10 @@
+package factorial;
+
+public class Example {
+	
+	public static void main(String[] args) {
+		System.out.println("Java" + 10 + 20);
+		System.out.println(10 + 20 + "Java");
+	}
+
+}
